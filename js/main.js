c4fefc9128a1bf64e5292a754b89755eb5af00b9
lau00050 <phil.lauricella@gmail.com>
2018-08-21 19:05:45 +1000

    Version2.2
    Added 'change to lowercase' event listener to the city variable

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $('#submit-btn').click(function() {
 	event.preventDefault()
-	var city = $('#city-type').val()
-	if (city === 'NYC' || city === 'New York City') {
+	var city = $('#city-type').val().toLowerCase()
+	if (city === 'nyc' || city === 'new york city') {
 		$('body').css('background-image','url(images/nyc.jpg)')
-	} else if (city === 'SF' || city === 'San Francisco') {
+	} else if (city === 'sf' || city === 'san francisco') {
 		$('body').css('background-image','url(images/sf.jpg)')
-	} else if (city === 'LA' || city === 'Los Angeles')	{
+	} else if (city === 'la' || city === 'los angeles')	{
 		$('body').css('background-image','url(images/la.jpg)')
-	} else if (city === 'ATX' || city === 'Austin') {
+	} else if (city === 'atx' || city === 'austin') {
 		$('body').css('background-image','url(images/austin.jpg)')
-	} else if (city === 'SYD' || city === 'Sydney') {
+	} else if (city === 'syd' || city === 'sydney') {
 		$('body').css('background-image','url(images/sydney.jpg)')
 	} else {
 		$('body').css('background-image','url(images/citipix_skyline.jpg)')
